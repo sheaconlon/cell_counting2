87000e233cf16d3a87df4097f1f57ad997071d15
Shea Conlon <sheaconlon@users.noreply.github.com>
2017-11-27 18:14:36 +0000

    partially fix class-diff loss

--- src/losses.py
+++ src/losses.py
@@ -1,3 +1,5 @@
+import tensorflow as tf
+
 def make_class_difference_loss(class_fn=lambda x: x, exp=1):
 	"""Make a class-difference loss function.
 
@@ -8,19 +10,20 @@ def make_class_difference_loss(class_fn=lambda x: x, exp=1):
 	y is the softmax of the model's output for an example (a vector)
 
 	Args:
-		class_fn (func(int) -> int): A function that takes a class number and
-			returns a number. Applied to the class numbers before they are used
-			in the calculation. Default is the identity function.
+		class_fn (func(tf.Tensor) -> tf.Tensor): A function that takes class
+			numbers and returns numbers. Applied to the class numbers before
+			they are used in the calculation. Default is the identity function.
 		exp (float): The exponent on the class difference. Default is 1.
 	"""
-	def class_difference_loss(pred, actual):
-		pred_cls = np.argmax(pred, axis=1)
-		actual_cls = np.argmax(actual, axis=1)
-		for i in range(pred_clas.shape[0]):
-			pred_cls[i] = class_fn(pred_cls[i])
-			actual_cls[i] = class_fn(actual_cls[i])
-		diffs = pred_cls - actual_cls
-		diffs = np.absolute(diffs)
-		diffs = np.power(diffs, exp)
-		return diffs
+	def class_difference_loss(actual, pred):
+		pred_cls = tf.argmax(pred, axis=1)
+		actual_cls = tf.argmax(actual, axis=1)
+
+		pred_cls = class_fn(pred_cls)
+		actual_cls = class_fn(actual_cls)
+
+		diffs = tf.subtract(pred_cls, actual_cls)
+		diffs = tf.abs(diffs)
+		diffs = tf.pow(diffs, exp)
+		return tf.reduce_sum(diffs, name="loss")
 	return class_difference_loss
