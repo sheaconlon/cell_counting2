fe71977aef6b4a297c7fa745f0b34353fc0ebf3d
Shea Conlon <sheaconlon@users.noreply.github.com>
2018-03-11 19:43:44 -0700

    add single source for version

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
 from distutils.core import setup
 
+VERSION = '0.1.2-alpha'
+
 setup(
   name = 'cell_counting',
   packages = ['cell_counting'],
-  version = '0.1.1',
+  version = VERSION,
   description = 'Tools for counting cell colonies on plates of growth medium.',
   author = 'Shea Conlon',
   author_email = 'sheaconlon@berkeley.edu',
   url = 'https://github.com/sheaconlon/cell_counting',
-  download_url = 'https://github.com/sheaconlon/cell_counting/archive/v0.09-alpha.tar.gz',
+  download_url = 'https://github.com/sheaconlon/cell_counting/archive/v{0:s}'
+                 '-alpha.tar.gz'.format(VERSION),
   keywords = ['cell', 'colony', 'counting', 'automated', 'machine learning'],
   classifiers = [],
   install_requires=[
