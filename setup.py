115b072dc217023bccd34b5989a707d98d381370
Shea Conlon <sheaconlon@users.noreply.github.com>
2018-03-11 19:39:59 -0700

    remove commented old version

--- setup.py
+++ setup.py
@@ -1,11 +1,3 @@
-# from setuptools import setup, find_packages
-#
-# setup(
-#     name='cell_counting',
-#     version='0.1',
-#     description='A sample Python project',
-#     packages=find_packages()
-# )
 from distutils.core import setup
 
 setup(
